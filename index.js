905b76622197bfe20848798f38f51df129e78240
blue shadow <anujose1989@gmail.com>
2017-10-10 05:38:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber (katzDeliLine, name) {
    return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing (katzDeliLine){
+//function nowServing (katzDeliLine){
 function nowServing (){
   if (katzDeliLine.length == 0){
     return "There is nobody waiting to be served!";
